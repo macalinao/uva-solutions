936ab9610879b045071ec9d3ed55e2876270ad81
Ian Macalinao <me@ian.pw>
2015-03-17 17:55:26 -0500

    Improve. I am better than @Scyphi.

--- 100/Main.java
+++ 100/Main.java
@@ -3,6 +3,8 @@ import java.util.*;
 
 public class Main {
 
+    static int[] dp = new int[10000000];
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
@@ -18,14 +20,23 @@ public class Main {
     }
 
     public static int cycleLen(int n) {
-        int len = 1;
-        while (n != 1) {
-            if (n % 2 != 0) {
-                n = 3 * n + 1;
-            } else {
-                n = n / 2;
-            }
-            len++;
+        if (n < dp.length && dp[n] != 0) {
+            return dp[n];
+        }
+
+        if (n == 1) {
+            return 1;
+        }
+
+        int len;
+        if (n % 2 != 0) {
+            len = cycleLen(3 * n + 1) + 1;
+        } else {
+            len = cycleLen(n / 2) + 1;
+        }
+
+        if (n < dp.length) {
+            dp[n] = len;
         }
         return len;
     }
